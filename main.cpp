56e52871de73b196e706d37bc3814ef5bf02e0f9
Aleksei Nikitenko <thenikitenko@gmail.com>
2018-03-07 09:31:13 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "md5.cpp"
+#include <string>
+#include <conio.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <Windows.h>
+#include <winuser.h>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <complex>
+#include <string>
+using namespace std;
+
+void toClipboard(HWND hwnd, const std::string &s);
+int main(int argc, char *argv[])
+{
+	string userString;
+	string md5String;
+    cout << "Write your string here: "<< endl;
+    cin>>userString;
+	md5String=md5(userString);
+	cout<<md5String;
+	HWND hwnd = GetDesktopWindow();
+	toClipboard(hwnd, md5String);
+	cout << endl<<"String saved to clipboard";
+	cout << endl<<"Press enter to exit";
+	getch();
+    return 0;
+}
+void toClipboard(HWND hwnd, const std::string &s){
+	OpenClipboard(hwnd);
+	EmptyClipboard();	
+	HGLOBAL hg=GlobalAlloc(GMEM_MOVEABLE,s.size()+1);
+	if (!hg){
+		CloseClipboard();
+		return;
+	}
+	memcpy(GlobalLock(hg),s.c_str(),s.size()+1);
+	GlobalUnlock(hg);
+	SetClipboardData(CF_TEXT,hg);
+	CloseClipboard();
+	GlobalFree(hg);
+}
